c1c614360b9ff389a619d25f851e821c092992f2
mediv0 <anonymousaxis@gmail.com>
2021-03-06 00:05:33 +0330

    Create development env configs

---
+++ build/rollup.config.js
@@ -0,0 +1,18 @@
+// /* eslint-disable */
+// /*
+// import { terser } from "rollup-plugin-terser";
+// import { babel } from "@rollup/plugin-babel";
+// import commonjs from "@rollup/plugin-commonjs";
+// import resolve from "@rollup/plugin-node-resolve";
+// import pkg from "../package.json";
+// import replace from "@rollup/plugin-replace";
+
+
+// const banner = `/*!
+//   * {name} v${pkg.version} 🖖 (WEB)
+//   * Forged by Mediv0
+//   * Released under the MIT License.
+//   * © 2021, Mediv0. ({WEB})
+//   */`;
+
+// */
