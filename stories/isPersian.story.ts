2c54a72f779f794ad1891cfe8a2c73241740bcd1
mediv0 <anonymousaxis@gmail.com>
2021-03-10 19:21:12 +0330

    stories(isPersian): update (82334bf)

--- stories/isPersian.story.ts
+++ stories/isPersian.story.ts
@@ -3,25 +3,45 @@ import isPersian from "../src/modules/isPersian/index";
 const Template = (args: any) => ({
     components: { isPersian },
     setup() {
-        return { args: args.val };
+        // convert user input to regex (only for storybook)
+        const match = args.pattern.match(new RegExp("^/(.*?)/([gimy]*)$"));
+        const regex = new RegExp(match[1], match[2]);
+        return {
+            text: args.val,
+            trim: regex
+        };
     },
-    template: '<is-persian :args="args" v-slot="{isPersian}">is given text persian: <b>{{ isPersian }}</b></is-persian>'
+    template: '<is-persian :str="text" :trimPattern="trim" v-slot="{isPersian}">is given text persian: <b>{{ isPersian }}</b></is-persian>'
 });
 
 export const Default = Template.bind({});
-Default.args = { val: "این یک متن فارسی است" };
+Default.args = { val: "این یک متن فارسی است", pattern: `/["'-+()؟\s.]/g` };
 
 export default {
     title: "Modules/isPersian",
     component: isPersian,
     argTypes: {
         val: {
-            name: "args",
+            name: "str",
             type: { name: "string", required: true },
             defaultValue: null,
             description: "checks if given value is persian or not",
             table: {
                 type: { summary: "string" },
+                defaultValue: { summary: "null" }
+            },
+            control: {
+                type: "text"
+            }
+        },
+
+        pattern: {
+            name: "trimPattern",
+            type: { name: "string", required: false },
+            defaultValue: null,
+            description: "Pattern of characters which you want to trim from the string e.g. '-+. ()'",
+            table: {
+                type: { summary: "RegExp" },
                 defaultValue: { summary: "null" }
             },
             control: {
