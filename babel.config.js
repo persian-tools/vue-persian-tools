380c7c78d6429cf3b8ba5f03c1e05f8cafcee13d
mediv0 <anonymousaxis@gmail.com>
2021-03-10 04:39:19 +0330

    build: update babel presents for rollup build

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
-module.exports = {
+const dev = {
     presets: ["@vue/cli-plugin-babel/preset"]
 };
+const prod = {
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                targets: {
+                    esmodules: true
+                }
+            }
+        ],
+        "@babel/preset-typescript"
+    ]
+};
+
+// NODE_ENV test for jest
+module.exports = process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test" ? dev : prod;
