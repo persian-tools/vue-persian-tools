7b3fe4f979305a78b4c8c244cf9586babfd1c3de
mediv0 <anonymousaxis@gmail.com>
2021-03-10 04:45:18 +0330

    build: export vue components on Vue.use

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Plugin } from "@vue/runtime-core";
+import * as components from "./modules/index";
+
+const install: Plugin = Vue => {
+    Object.values(components).forEach(cmp => {
+        Vue.component(cmp.name, cmp);
+    });
+};
+
+export default install;
+
+// export components
+export { default as isPersian } from "./modules/isPersian";
