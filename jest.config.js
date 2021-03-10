53f76a92d162d524a869ce68134184cfc8d92580
mediv0 <anonymousaxis@gmail.com>
2021-03-10 04:41:40 +0330

    build: update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
     preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
     transform: {
         "^.+\\.vue$": "vue-jest"
-    }
+    },
+    coverageDirectory: "coverage",
+    collectCoverageFrom: ["./src/modules/**/*.ts", "!./src/modules/index.ts", "!./src/modules/helper.ts"],
+    collectCoverage: true
 };
