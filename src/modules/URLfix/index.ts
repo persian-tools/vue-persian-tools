f2bbbe15a1c2ba1cdcf85821bacc62265272a4e2
mediv0 <anonymousaxis@gmail.com>
2021-03-10 20:39:40 +0330

    feat: add URLfix module

---
+++ src/modules/URLfix/index.ts
@@ -0,0 +1,25 @@
+import { defineComponent, h } from "vue-demi";
+import { URLfix } from "@persian-tools/persian-tools";
+import { getSlot } from "../helpers/getSlot.skip";
+import tag from "../helpers/tagPropMixin.skip";
+
+export default defineComponent({
+    name: "URLfix",
+    mixins: [tag],
+    props: {
+        url: {
+            type: String,
+            requied: true
+        }
+    },
+    computed: {
+        fixURL(): string | undefined {
+            return URLfix(this.url!);
+        }
+    },
+
+    render() {
+        const __SLOTS__ = getSlot(this, { url: this.fixURL });
+        return h(this.tag, __SLOTS__);
+    }
+});
