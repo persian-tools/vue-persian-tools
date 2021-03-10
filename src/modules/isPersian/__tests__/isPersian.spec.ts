4c7cac70130f9f558c36abbf44caddf50596ec73
mediv0 <anonymousaxis@gmail.com>
2021-03-10 04:46:37 +0330

    test: add isPersian tests

---
+++ src/modules/isPersian/__tests__/isPersian.spec.ts
@@ -0,0 +1,48 @@
+import { mount } from "@vue/test-utils";
+import isPersian from "../index";
+
+describe("isPersian", () => {
+    const cmp = {
+        props: ["txt"],
+        components: {
+            fa: isPersian
+        },
+        template: "<div><fa :text=txt v-slot='{isPersian}'>{{ isPersian }}</fa></div>"
+    };
+    it("should return true if is given text is Persian", async () => {
+        const wrapper = mount(cmp, {
+            propsData: {
+                txt: "متن فارسی"
+            }
+        });
+        expect(wrapper.html()).toMatch("true");
+    });
+
+    it("should return false if is given text is not Persian", async () => {
+        const wrapper1 = mount(cmp, {
+            propsData: {
+                txt: "english text"
+            }
+        });
+        expect(wrapper1.html()).toMatch("false");
+
+        const wrapper2 = mount(cmp, {
+            propsData: {
+                txt: "こんにちは世界"
+            }
+        });
+        expect(wrapper2.html()).toMatch("false");
+    });
+
+    it("should render isPersian with given html tag", () => {
+        const cmp = {
+            components: {
+                fa: isPersian
+            },
+            template: "<div><fa tag='p' text='random text' v-slot='{isPersian}'>{{ isPersian }}</fa></div>"
+        };
+
+        const wrapper = mount(cmp);
+        expect(wrapper.html()).toMatch("<p>false</p>");
+    });
+});
