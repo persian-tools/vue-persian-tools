059f8e0a83f5a14d9f499d2b769c8a146f7b1dd2
mediv0 <anonymousaxis@gmail.com>
2021-03-17 01:18:33 +0330

    test(toPersianChars): change spec titles

--- src/modules/toPersianChars/__tests__/toPersianChars.spec.ts
+++ src/modules/toPersianChars/__tests__/toPersianChars.spec.ts
@@ -1,7 +1,7 @@
 import { mount } from "@vue/test-utils";
 import toPersianChars from "../index";
 
-describe("hasPersian", () => {
+describe("toPersianChars", () => {
     const cmp = {
         props: ["val"],
         components: {
@@ -9,7 +9,7 @@ describe("hasPersian", () => {
         },
         template: "<div><toPersianChars :str=val v-slot='{ text }'>{{ text }}</toPersianChars></div>"
     };
-    it("should return true if text contains persian values", () => {
+    it("should convert to persian char", () => {
         const wrapper = mount(cmp, {
             propsData: {
                 val: "علي"
