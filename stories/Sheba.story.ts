8248a24d88614a556c180aaad05c8af5b874bf6d
mediv0 <anonymousaxis@gmail.com>
2021-03-17 02:36:32 +0330

    stories: add Sheba story

---
+++ stories/Sheba.story.ts
@@ -0,0 +1,51 @@
+import Sheba from "../src/modules/Sheba/index";
+
+const Template = (args: any) => ({
+    components: { Sheba },
+    setup() {
+        return {
+            code: args.val
+        };
+    },
+    template: `
+    <Sheba :shebaCode="code" v-slot="{ isValid, info }">
+        is sheba valid: <b>{{ isValid }}</b>
+        <br />
+        <br />
+        <h3>info about sheba</h3>
+        <div v-if="info">
+            <p> nickname: {{ info.nickname }} </p>
+            <p> name: {{ info.name }} </p>
+            <p> persianName: {{ info.persianName }} </p>
+            <p> code: {{ info.code }} </p>
+            <p> accountNumberAvailable: {{ info.accountNumberAvailable }} </p>
+        </div>
+        <div v-else>
+            invalid
+        </div>
+    </Sheba>
+    `
+});
+
+export const Default = Template.bind({});
+Default.args = { val: "IR820540102680020817909002" };
+
+export default {
+    title: "Modules/Sheba",
+    component: Sheba,
+    argTypes: {
+        val: {
+            name: "shebaCode",
+            type: { name: "string", required: true },
+            defaultValue: null,
+            description: "get info & validate iranian Sheba(IBAN)",
+            table: {
+                type: { summary: "string" },
+                defaultValue: { summary: "null" }
+            },
+            control: {
+                type: "text"
+            }
+        }
+    }
+};
