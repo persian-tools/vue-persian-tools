713ba4e7f1037f85e78958b253af8558329f423d
mediv0 <anonymousaxis@gmail.com>
2021-03-16 23:27:29 +0330

    stories: add toPersianChars story

---
+++ stories/toPersianChars.story.ts
@@ -0,0 +1,34 @@
+import toPersianChars from "../src/modules/toPersianChars/index";
+
+const Template = (args: any) => ({
+    components: { toPersianChars },
+    setup() {
+        return {
+            val: args.val
+        };
+    },
+    template: '<toPersianChars :str="val" v-slot="{ text }">converted text: <b>{{ text }}</b></toPersianChars>'
+});
+
+export const Default = Template.bind({});
+Default.args = { val: "علي" };
+
+export default {
+    title: "Modules/toPersianChars",
+    component: toPersianChars,
+    argTypes: {
+        val: {
+            name: "str",
+            type: { name: "string", required: true },
+            defaultValue: null,
+            description: "Description: Replaces all instances of ي and ك withی and ک, respectively. It should not make any ch anges to Arabic text surrounded by appropriate templates.",
+            table: {
+                type: { summary: "string" },
+                defaultValue: { summary: "null" }
+            },
+            control: {
+                type: "text"
+            }
+        }
+    }
+};
