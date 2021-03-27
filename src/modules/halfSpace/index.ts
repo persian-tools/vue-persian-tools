6e3140921a8964e685435477763393c9cc6437e6
mediv0 <anonymousaxis@gmail.com>
2021-03-28 02:03:27 +0430

    feat: add halfSpace module

---
+++ src/modules/halfSpace/index.ts
@@ -0,0 +1,25 @@
+import { defineComponent, h } from "vue-demi";
+import { halfSpace } from "@persian-tools/persian-tools";
+import { getSlot } from "../helpers/getSlot.skip";
+import tag from "../helpers/tagPropMixin.skip";
+
+export default defineComponent({
+    name: "halfSpace",
+    mixins: [tag],
+    props: {
+        str: {
+            type: String,
+            requied: true
+        }
+    },
+    computed: {
+        computeHalfspace(): string | undefined {
+            return halfSpace(this.str!);
+        }
+    },
+
+    render() {
+        const __SLOTS__ = getSlot(this, { text: this.computeHalfspace });
+        return h(this.tag, __SLOTS__);
+    }
+});
