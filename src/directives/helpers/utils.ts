c0ee76301286c303c7114b31c8eb9511027d5460
Alireza Alibeiki <alibeikialireza@gmail.com>
2021-04-28 08:46:38 +0430

    fix: fix Directive Interface

--- src/directives/helpers/utils.ts
+++ src/directives/helpers/utils.ts
@@ -3,12 +3,12 @@ import { ObjectDirective as Vue3Directive, DirectiveBinding as Vue3DirectiveBind
 interface Vue2DirectiveBinding {
     modifiers: Record<string, boolean>;
 }
-interface Vue2Directive<T = any> {
+interface Vue2Directive<T = HTMLInputElement> {
     bind?: (el: T, binding: Vue2DirectiveBinding) => void;
     unbind?: (el: T, binding: Vue2DirectiveBinding) => void;
 }
 
-type Directive = Vue2Directive | Vue3Directive;
+interface Directive<T = HTMLInputElement> extends Vue2Directive<T>, Vue3Directive {}
 type DirectiveBinding = Vue2DirectiveBinding | Vue3DirectiveBinding;
 
-export { Vue2DirectiveBinding, Vue2Directive, Vue3Directive, Directive, DirectiveBinding };
+export { Vue2DirectiveBinding, Vue2Directive, Vue3DirectiveBinding, Vue3Directive, Directive, DirectiveBinding };
