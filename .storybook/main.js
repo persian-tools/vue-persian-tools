f19f64d5bcf5cd22665ef53492a3f7b37285b44b
mediv0 <anonymousaxis@gmail.com>
2021-05-27 22:26:43 +0430

    build: update stories path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-    stories: ["../stories/*.story.mdx", "../stories/*.story.@(js|jsx|ts|tsx)"],
+    stories: ["../stories/**/*.story.mdx", "../stories/**/*.story.@(js|jsx|ts|tsx)"],
     addons: ["@storybook/addon-links", "@storybook/addon-essentials"]
 };
