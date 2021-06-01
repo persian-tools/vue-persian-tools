983def66f0302a2390749c81647509e478cd8789
Alireza Alibeiki <alibeikialireza@gmail.com>
2021-06-01 18:46:45 +0430

    chore(stories): update wordsToNumberDirective story

--- stories/directives/wordsToNumberDirective.story.ts
+++ stories/directives/wordsToNumberDirective.story.ts
@@ -4,13 +4,18 @@ const Template = (args: any) => ({
     directives: { wordsToNumber: wordsToNumber },
     setup() {
         return {
-            text: args.val
+            text: args.val,
+            digits: args.digits
         };
     },
     template: `<div>
         <div><strong>Text:</strong> {{text}}</div>
         <br />
-        <div><strong>Number:</strong> <span v-words-to-number>{{text}}</span></div>
+        <div><strong>Number:</strong> <span v-words-to-number:[digits]>{{text}}</span></div>
+        <br />
+        <div><strong>Number with add Comma:</strong> <span v-words-to-number:[digits].addCommas>{{text}}</span></div>
+        <br />
+        <div><strong>Number with fuzzy modifier:</strong> <span v-words-to-number:[digits].fuzzy>{{text}}</span></div>
     </div>`
 });
 
@@ -31,6 +36,20 @@ export default {
             },
             control: {
                 type: "text"
+            }
+        },
+        digits: {
+            name: "digits",
+            type: { name: "string", required: true },
+            defaultValue: null,
+            description: "Convert words to digits in given language.",
+            table: {
+                type: { summary: "fa | en | ar" },
+                defaultValue: { summary: "null" }
+            },
+            control: {
+                type: "radio",
+                options: ["en", "fa", "ar"]
             }
         }
     }
